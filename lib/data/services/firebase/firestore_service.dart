3af4e5e321e1b205b9c236031323b6dd579265c7
ZhekaGila <gilazhevzhandos@gmail.com>
2026-05-14 18:34:25 +0500

    adding firestore service

---
+++ lib/data/services/firebase/firestore_service.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../models/product_model.dart';
+
+class FirestoreService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> saveOrder(List<ProductModel> products) async {
+    final total = products.fold<double>(0, (sum, item) => sum + item.price);
+
+    await _firestore.collection('orders').add({
+      'date': DateTime.now().toIso8601String(),
+
+      'total': total,
+
+      'products': products
+          .map((p) => {'title': p.title, 'price': p.price})
+          .toList(),
+    });
+  }
+
+  Stream<QuerySnapshot> getOrders() {
+    return _firestore.collection('orders').snapshots();
+  }
+}
