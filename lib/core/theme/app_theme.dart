77c925423306ca257d0bb81e49878e5e9cce5e5a
ZhekaGila <gilazhevzhandos@gmail.com>
2026-05-14 23:35:01 +0500

    custom theme

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    brightness: Brightness.light,
+    useMaterial3: true,
+  );
+
+  static final darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    useMaterial3: true,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'ui/providers/theme_provider.dart';
 import 'core/router/app_router.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'core/theme/app_theme.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -19,8 +19,11 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
+      title: 'Super Puper eCommerce Market Place App',
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
     );
   }
 }
